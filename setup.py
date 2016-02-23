69965a55df8743fbccbad709e4f76b05d7bd9785
Gil Forsyth <gilforsyth@gmail.com>
2016-02-23 17:06:01 -0500

    add setup.py installer

---
+++ setup.py
@@ -0,0 +1,18 @@
+import sys
+from setuptools import setup, find_packages, Extension
+
+
+def main():
+    if sys.version_info[0] != 2:
+        sys.exit('PyGBe only supports Python 2.7')
+
+    setupkw = dict(
+            name='PyGBe',
+            description='A boundary element method code that does molecular electrostatics calculations with a continuum approach',
+            platforms='Linux',
+            packages = find_packages(),
+            )
+    setup(**setupkw)
+
+if __name__ == '__main__':
+    main()
