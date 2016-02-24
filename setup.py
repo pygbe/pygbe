21d0fddfb87cbf739cc25a123b0f8fb6e0e66ece
Gil Forsyth <gilforsyth@gmail.com>
2016-02-24 14:14:32 -0500

    finish setup.py setup (WOOHOO!!!)
    This was a *beast*.  Still work to be done, but this completely removes
    the need for a makefile.  The `setuptools` library has tools for
    building and linking SWIG modules and we also don't have to do any of
    the insane acrobatics in make to find the location of the numpy include
    directory, since we can just use `numpy.get_include()`

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import sys
 from setuptools import setup, find_packages, Extension
+import numpy
+
 def main():
     if sys.version_info[0] != 2:
         sys.exit('PyGBe only supports Python 2.7')
@@ -9,6 +11,32 @@ def main():
             description='A boundary element method code that does molecular electrostatics calculations with a continuum approach',
             platforms='Linux',
             packages = find_packages(),
+            ext_modules = [
+                Extension("_multipole",
+                          sources=["pygbe/tree/multipole.i", "pygbe/tree/multipole.cpp"],
+                          swig_opts=['-c++'],
+                          include_dirs=[numpy.get_include()],
+                          extra_compile_args=['-fPIC', '-O3', '-funroll-loops', '-msse3', '-fopenmp'],
+                ),
+                Extension("_direct",
+                          sources=["pygbe/tree/direct.i", "pygbe/tree/direct.cpp"],
+                          swig_opts=['-c++'],
+                          include_dirs=[numpy.get_include()],
+                          extra_compile_args=['-fPIC', '-O3', '-funroll-loops', '-msse3', '-fopenmp'],
+                ),
+                Extension("_calculateMultipoles",
+                          sources=["pygbe/tree/calculateMultipoles.i", "pygbe/tree/calculateMultipoles.cpp"],
+                          swig_opts=['-c++'],
+                          include_dirs=[numpy.get_include()],
+                          extra_compile_args=['-fPIC', '-O3', '-funroll-loops', '-msse3', '-fopenmp'],
+                ),
+                Extension("_semi_analyticalwrap",
+                          sources=["pygbe/util/semi_analyticalwrap.i", "pygbe/util/semi_analyticalwrap.cpp"],
+                          swig_opts=['-c++'],
+                          include_dirs=[numpy.get_include()],
+                          extra_compile_args=['-fPIC', '-O3', '-funroll-loops', '-msse3', '-fopenmp'],
+                ),
+                ]
             )
     setup(**setupkw)
 
