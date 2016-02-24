d63c0449aa62a8504f573a64e1dbc28e8d0bcd8e
Gil Forsyth <gilforsyth@gmail.com>
2016-02-24 14:56:27 -0500

    add pygbe shortcut to pythonpath BIN

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
 import sys
+from distutils.command.build import build
+from setuptools.command.install import install
 from setuptools import setup, find_packages, Extension
 import numpy
+
+class CustomBuild(build):
+    """
+    Subclasses build command to ensure that built_ext is run before files
+    are copied.  Make setuptools compile the SWIG files first, then run install
+    """
+    def run(self):
+        self.run_command('build_ext')
+        build.run(self)
+
+class CustomInstall(install):
+    """
+    Subclasses install command to ensure that built_ext is run before files
+    are copied.  Make setuptools compile the SWIG files first, then run install
+    """
+    def run(self):
+        self.run_command('build_ext')
+        self.do_egg_install()
 
 def main():
     if sys.version_info[0] != 2:
@@ -10,7 +30,17 @@ def main():
             name='PyGBe',
             description='A boundary element method code that does molecular electrostatics calculations with a continuum approach',
             platforms='Linux',
+            install_requires = [
+                'pycuda >= 2013.1',
+                'numpy > 1.8',
+            ],
+            license='MIT',
             packages = find_packages(),
+            #tell setuptools to use the custom build and install classes
+            cmdclass={'build': CustomBuild, 'install': CustomInstall},
+            #create an entrance point that points to pygbe.main.main
+            entry_points={'console_scripts': ['pygbe = pygbe.main:main']},
+            #SWIG modules with all compilation options
             ext_modules = [
                 Extension("_multipole",
                           sources=["pygbe/tree/multipole.i", "pygbe/tree/multipole.cpp"],
@@ -36,7 +66,7 @@ def main():
                           include_dirs=[numpy.get_include()],
                           extra_compile_args=['-fPIC', '-O3', '-funroll-loops', '-msse3', '-fopenmp'],
                 ),
-                ]
+                ],
             )
     setup(**setupkw)
 
