059377d853af78f54893a0f15e57b7e665087fcd
Gil Forsyth <gilforsyth@gmail.com>
2016-05-10 13:51:15 -0400

    move regression framework to separate file and PICKLETIME
    added pickle routines to save the results after each individual run
    within a given test, so when we get to other tests that require the same
    run, they can just load those results from cache and not re-run an
    entire set of cases for 5 progressively larger meshes
    
    
    Former-commit-id: 7b67a225d01888e2bfc970c708af4ceddc4b6199

--- tests/regression_tests/molecule_dirichlet.py
+++ tests/regression_tests/molecule_dirichlet.py
@@ -1,122 +1,47 @@
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
-import os
 import numpy
-import shutil
-import re
-import sys
-import math
+import pickle
+
 from pygbe.util import an_solution
-from pygbe.main import main as pygbe
-
-ITER_REGEX = re.compile('Converged after (\d*) iterations')
-N_REGEX = re.compile('Total elements : (\d*)')
-ESOLV_REGEX = re.compile('Esolv = (\-*\d*\.\d*)\ kcal\/mol')
-ESURF_REGEX = re.compile('Esurf = (\-*\d*\.\d*)\ kcal\/mol')
-ECOUL_REGEX = re.compile('Ecoul = (\-*\d*\.\d*)\ kcal\/mol')
-TIME_REGEX = re.compile('Time = (\-*\d*\.\d*)\ s')
-
-def scanOutput(filename):
-
-    with open(filename, 'r') as f:
-        txt = f.read()
-
-        N= re.search(N_REGEX, txt)
-        if N:
-            N = int(N.group(1))
-        iterations = re.search(ITER_REGEX, txt)
-        if iterations:
-            iterations = int(iterations.group(1))
-        Esolv = re.search(ESOLV_REGEX, txt)
-        if Esolv:
-            Esolv = float(Esolv.group(1))
-        Esurf = re.search(ESURF_REGEX, txt)
-        if Esurf:
-            Esurf = float(Esurf.group(1))
-        Ecoul = re.search(ECOUL_REGEX, txt)
-        if Ecoul:
-            Ecoul = float(Ecoul.group(1))
-        Time = re.search(TIME_REGEX, txt)
-        if Time:
-            Time = float(Time.group(1))
-
-
-    return N, iterations, Esolv, Esurf, Ecoul, Time
-
-
-
-def run_regression(mesh, test_name, problem_folder, param, delete_output=True):
-    """
-    Runs regression tests over a series of mesh sizes
-
-    Inputs:
-    ------
-        mesh: array of mesh suffixes
-        problem_folder: str name of folder containing meshes, etc...
-        param: str name of param file
-
-    Returns:
-    -------
-        N: len(mesh) array of elements of problem
-        iterations: # of iterations to converge
-        Esolv: array of solvation energy
-        Esurf: array of surface energy
-        Ecoul: array of coulomb energy
-        Time: time to solution (wall-time)
-    """
-    print 'Runs for molecule + set phi/dphi surface'
-    N = numpy.zeros(len(mesh))
-    iterations = numpy.zeros(len(mesh))
-    Esolv = numpy.zeros(len(mesh))
-    Esurf = numpy.zeros(len(mesh))
-    Ecoul = numpy.zeros(len(mesh))
-    Time = numpy.zeros(len(mesh))
-    for i in range(len(mesh)):
-        print 'Start run for mesh '+mesh[i]
-        outfile = pygbe(['',
-                         '-p', '{}'.format(param),
-                         '-c', '{}_{}.config'.format(test_name, mesh[i]),
-                         '-o', 'output_{}_{}'.format(test_name, mesh[i]),
-                         '-g', '../../pygbe/',
-                         '{}'.format(problem_folder),], return_output_fname=True)
-
-        print 'Scan output file'
-        outfolder = os.path.join('{}'.format(problem_folder),
-                                 'output_{}_{}'.format(test_name, mesh[i]))
-        outfile = os.path.join(outfolder, outfile)
-        N[i],iterations[i],Esolv[i],Esurf[i],Ecoul[i],Time[i] = scanOutput(outfile)
-        if delete_output:
-            shutil.rmtree(outfolder)
-
-
-    return(N, iterations, Esolv, Esurf, Ecoul, Time)
-
+from regression import scanOutput, run_regression, picklesave, pickleload
 
 def main():
-    test_outputs = {}
+    try:
+        test_outputs = pickleload()
+    except IOError:
+        test_outputs = {}
+
     problem_folder = 'input_files'
+    mesh = ['500','2K']#,'8K','32K','130K']
 
     #molecule_dirichlet
-    mesh = ['500','2K']#,'8K','32K','130K']
     param = 'sphere_fine.param'
     test_name = 'molecule_dirichlet'
-    N, iterations, Esolv, Esurf, Ecoul, Time = run_regression(mesh, test_name, problem_folder, param)
-    test_outputs[test_name] = [N, iterations, Esolv, Esurf, Ecoul, Time]
+    if test_name not in test_outputs.keys():
+        N, iterations, Esolv, Esurf, Ecoul, Time = run_regression(mesh, test_name, problem_folder, param)
+        test_outputs[test_name] = [N, iterations, Esolv, Esurf, Ecoul, Time]
+
+    picklesave(test_outputs)
 
     #molecule_single_center
-    mesh = ['500','2K']#,'8K','32K','130K']
     param = 'sphere_fine.param'
     test_name = 'molecule_single_center'
-    N, iterations, Esolv, Esurf, Ecoul, Time= run_regression(mesh, test_name, problem_folder, param)
-    test_outputs[test_name] = [N, iterations, Esolv, Esurf, Ecoul, Time]
+    if test_name not in test_outputs.keys():
+        N, iterations, Esolv, Esurf, Ecoul, Time= run_regression(mesh, test_name, problem_folder, param)
+        test_outputs[test_name] = [N, iterations, Esolv, Esurf, Ecoul, Time]
+
+    picklesave(test_outputs)
 
     #dirichlet_surface
-    mesh = ['500','2K']#,'8K','32K','130K']
     param = 'sphere_fine.param'
     test_name = 'dirichlet_surface'
-    N, iterations, Esolv, Esurf, Ecoul, Time= run_regression(mesh, test_name, problem_folder, param)
-    test_outputs[test_name] = [N, iterations, Esolv, Esurf, Ecoul, Time]
+    if test_name not in test_outputs.keys():
+        N, iterations, Esolv, Esurf, Ecoul, Time= run_regression(mesh, test_name, problem_folder, param)
+        test_outputs[test_name] = [N, iterations, Esolv, Esurf, Ecoul, Time]
+
+    picklesave(test_outputs)
 
     Esolv, Esurf, Ecoul = test_outputs['molecule_dirichlet'][2:5]
     Esolv_mol, Esurf_mol, Ecoul_mol = test_outputs['molecule_single_center'][2:5]
@@ -124,6 +49,8 @@ def main():
     Time = test_outputs['molecule_dirichlet'][-1]
     Time_mol = test_outputs['molecule_single_center'][-1]
     Time_surf = test_outputs['dirichlet_surface'][-1]
+    N = test_outputs['molecule_dirichlet'][0]
+    iterations = test_outputs['molecule_dirichlet'][1]
 
     Einter = Esolv + Esurf + Ecoul - Esolv_surf - Esurf_mol - Ecoul_mol - Esolv_mol - Esurf_surf - Ecoul_surf
     total_time = Time+Time_mol+Time_surf
