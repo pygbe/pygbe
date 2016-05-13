5afacdc2b9d4301717845f1e4a7d278dc4a7ae5e
Natalia C. Clementi <natyclementi@gmail.com>
2016-05-12 20:57:48 -0400

    Fixing header explanation and import *, for generateStern
    Former-commit-id: d50236cad0a1edc09f47c82d1572d2258d0bde89

--- scripts/generateStern.py
+++ scripts/generateStern.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 """
-It reads the .pqr file to generate the input for MSMS in order to
+It reads a .xyzr file that contains the positions of the atoms (x,y,z) and the
+van der Waals radius (r), to generate the input for MSMS in order to
 generate the mesh for the Stern layer.
+
+file_in : filename.xyzr. Given as an argv when running the script.
+file_out: filename.stern
+
+Note: 
+-----
+You can create the .xyzr file from the .pqr, extracting the columns 6, 7, 8, 10
+One possibility is type in the terminal:
+
+awk '{print $6, $7, $8, $10}' filename.pqr > filename.xyzr
 """
 import sys
-from numpy import *
+import numpy
 
 file_in = sys.argv[1]
 file_out = file_in+'.stern'
 
-X = loadtxt(file_in)
+X = numpy.loadtxt(file_in)
 
 X[:,3] += 2
 
-savetxt(file_out, X, fmt='%5.5f')
+numpy.savetxt(file_out, X, fmt='%5.5f')
