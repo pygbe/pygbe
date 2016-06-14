0c4caf185135e3e30887784e9d0cc2d6112c6286
Gil Forsyth <gilforsyth@gmail.com>
2016-06-14 12:32:53 -0400

    remove test cache after reg. run and clear out dead code
    Former-commit-id: bed6559ed2076e7d08dd1f63ecdd915ccc35149c

--- tests/regression_tests/sphere_neumann.py
+++ tests/regression_tests/sphere_neumann.py
@@ -49,46 +49,6 @@ def main():
                    total_time,
                    energy_type='Total')
 
-#
-#    font = {'family':'serif','size':10}
-#    fig = plt.figure(figsize=(3,2), dpi=80)
-#    ax = fig.add_subplot(111)
-#    asymp = N[0]*error[0]/N
-#    ax.loglog(N, error, c='k', marker='o',ls=' ', mfc='w', ms=5, label='')
-#    ax.loglog(N, asymp, c='k', marker='None', ls=':', lw=0.8, label=None)
-#    plt.rc('font',**font)
-#    loc = (3*N[0]+N[1])/4
-#    tex_loc = numpy.array((loc,N[0]*error[0]/loc))
-#    tex_angle = math.atan2(numpy.log(abs(asymp[-1]-asymp[0])),numpy.log(abs(N[-1]-N[0])))*180/math.pi
-#    ax.text(tex_loc[0], tex_loc[1],r'N$^{-1}$',fontsize=8,rotation=tex_angle,rotation_mode='anchor')
-#    ax.set_ylabel('Relative error', fontsize=10)
-#    ax.set_xlabel('Number of elements', fontsize=10)
-#    fig.subplots_adjust(left=0.185, bottom=0.21, right=0.965, top=0.95)
-#    fig.savefig('regression_tests/figs/error_energy_neumann_surface.pdf',dpi=80,format='pdf')
-#
-#    fig = plt.figure(figsize=(3,2), dpi=80)
-#    ax = fig.add_subplot(111)
-#    asymp = N*numpy.log(N)*total_time[0]/(N[0]*numpy.log(N[0]))
-#    ax.loglog(N, total_time, c='k', marker='o',ls=' ', mfc='w', ms=5, label='')
-#    ax.loglog(N, asymp,c='k',marker='None',ls=':', lw=0.8, label=None)
-#    loc = (3*N[0]+N[1])/4
-#    tex_loc = numpy.array((loc, loc*numpy.log(loc)*total_time[0]/(N[0]*numpy.log(N[0]))))
-#    tex_angle = math.atan2(numpy.log(abs(asymp[-1]-asymp[0])),numpy.log(abs(N[-1]-N[0])))*180/math.pi
-#    ax.text(tex_loc[0], tex_loc[1], 'NlogN', fontsize=8,rotation=tex_angle, rotation_mode='anchor')
-#    plt.rc('font',**font)
-#    ax.set_ylabel('Total time [s]', fontsize=10)
-#    ax.set_xlabel('Number of elements', fontsize=10)
-#    fig.subplots_adjust(left=0.185, bottom=0.21, right=0.965, top=0.95)
-#    fig.savefig('regression_tests/figs/total_time_neumann_surface.pdf',dpi=80,format='pdf')
-#
-#    fig = plt.figure(figsize=(3,2), dpi=80)
-#    ax = fig.add_subplot(111)
-#    ax.semilogx(N, iterations, c='k', marker='o',ls=' ', mfc='w', ms=5, label='')
-#    plt.rc('font',**font)
-#    ax.set_ylabel('Iterations', fontsize=10)
-#    ax.set_xlabel('Number of elements', fontsize=10)
-#    fig.subplots_adjust(left=0.185, bottom=0.21, right=0.965, top=0.95)
-#    fig.savefig('regression_tests/figs/iterations_neumann_surface.pdf',dpi=80,format='pdf')
 
 if __name__ == "__main__":
     from check_for_meshes import check_mesh
