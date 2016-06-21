8ed658ea76585952572f858ecb8f580e6d3d92dc
Natalia C. Clementi <natyclementi@gmail.com>
2016-06-21 16:16:37 -0400

    Adding header description to main_maitrix

--- pygbe_matrix/main_matrix.py
+++ pygbe_matrix/main_matrix.py
@@ -1,25 +1,12 @@
-#!/usr/bin/env python
-'''
-  Copyright (C) 2013 by Christopher Cooper, Lorena Barba
+"""
+This code solves the multisurface BEM for proteins interacting with charged
+surfaces. 
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
+ -It returns the Solvation Energy, Surface Energy and Coulombic Energy. 
 
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-'''
+ -If we have an incident electric field, it returns the extinction cross
+  section. 
+"""
 
 # This code solves the multisurface BEM for proteins  
 # interacting with charged surfaces
