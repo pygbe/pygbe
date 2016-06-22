c5bb1ecf799fd7d5c0e046c662815aea8aad0ad8
Natalia C. Clementi <natyclementi@gmail.com>
2016-06-22 18:24:28 -0400

    Fixing import problem

--- pygbe_matrix/blockMatrixGen.py
+++ pygbe_matrix/blockMatrixGen.py
@@ -19,7 +19,7 @@
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
   THE SOFTWARE.
 '''
-
+import numpy
 from numpy              import *
 from class_definition   import *
 from scipy.sparse       import *
