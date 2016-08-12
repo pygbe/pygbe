bfe2002e7564c7b77b72e082192194cf10caefbf
Gil Forsyth <gilforsyth@gmail.com>
2016-08-12 11:17:11 -0400

    Update side-toolbar with better sphinx options
    Removed the explicit link at the top in favor of the template-driven
    version that also includes the project name.  Should also (hopefully)
    include a button linking back to the github repo.

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,12 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'description': 'Python, GPUs and Boundary elements for biomolecular electrostatics',
+    'github_user': 'barbagroup',
+    'github_repo': 'pygbe',
+    'github_button': 'true',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
