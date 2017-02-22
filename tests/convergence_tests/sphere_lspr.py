12f2fd83ecdbd52697c2ea60cd3c93355e2d7614
Natalia C. Clementi <natyclementi@gmail.com>
2017-02-22 17:26:58 -0500

    Add mesh_ratio to imports

--- tests/convergence_tests/sphere_lspr.py
+++ tests/convergence_tests/sphere_lspr.py
@@ -1,5 +1,5 @@
 from pygbe.util import an_solution
-from convergence_lspr import (run_convergence, picklesave, pickleload,
+from convergence_lspr import (mesh_ratio, run_convergence, picklesave, pickleload,
                               report_results, mesh)
 
 def main():
