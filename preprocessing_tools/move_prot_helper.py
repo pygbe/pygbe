086a761da0f29691a05e1fbb555ea25c6a2ac330
Naty Clementi <natyclementi@gmail.com>
2018-01-03 13:06:13 -0500

    Add script and helepr to move protein

---
+++ preprocessing_tools/move_prot_helper.py
@@ -0,0 +1,206 @@
+## This helper contains a set of functions used in move_protein script
+
+import numpy
+
+from argparse import ArgumentParser
+
+
+def read_inputs():
+    """
+    Parse command-line arguments to run move_protein.
+
+    User should provide:
+    -inMesh : str, mesh file you want to rotate.
+    -inpqr  : str, pqr of the object you want to rotate.
+    -alpha_y: float [degrees], rotation angle, about the dipole moment. 
+    -alpha_z: float [degrees], tilt angle.
+    -name   : str, output file name.
+    -verbose: bool, set to True if extra information is desired.
+    """
+
+    parser = ArgumentParser(description='Manage move_protein command line arguments')
+
+
+    parser.add_argument('-im', '--inMesh', dest='im', type=str, default=None,
+                        help="mesh file you want to rotate")
+
+    parser.add_argument('-ip', '--inpqr', dest='ip', type=str, default=None,
+                        help="pqr of the object you want to rotate")
+
+    parser.add_argument('-ay', '--alpha_y', dest='ay', type=float, default=None,
+                        help="rotation angle, about the dipole moment")
+
+    parser.add_argument('-az', '--alpha_z', dest='az', type=float, default=None,
+                        help="tilt angle")
+
+    parser.add_argument('-n', '--name', dest='name', type=str, default='',
+                        help="output file name")
+    
+    parser.add_argument('-v', '--verbose', dest='v', type=bool, default=False,
+                        help="set to True if extra information is desired")
+
+    return parser.parse_args()
+
+
+
+def read_vertex(filename, REAL):
+    """
+    It reads the vertex of the triangles from the mesh file and it stores
+    them on an array.
+
+    Arguments
+    ----------
+    filename: name of the file that contains the surface information.
+              (filename.vert)
+    REAL    : data type.
+
+    Returns
+    -------
+    vertex: array, vertices of the triangles.
+    """
+
+    X = numpy.loadtxt(filename, dtype=REAL)
+    vertex = X[:, 0:3]
+
+    return vertex
+
+def read_pqr(filename, REAL):
+    """
+    Read charge information from pqr file
+
+    Arguments
+    ----------
+    filename: name of the file that contains the surface information.
+               (filename.pqr)
+    REAL    : data type.
+
+    Returns
+    -------
+    pos     : (Nqx3) array, positions of the charges.
+    q       : (Nqx1) array, value of the charges.
+    Nq      : float, number of charges, length of array q. 
+    """
+
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    pos = []
+    q = []
+    for line in lines:
+        line = line.split()
+
+        if line[0] == 'ATOM':
+            #  grab coordinates and charge from columns
+            x, y, z, q0 = [REAL(i) for i in line[5:-1]]
+            q.append(q0)
+            pos.append([x, y, z])
+
+    pos = numpy.array(pos)
+    q = numpy.array(q)
+    Nq = len(q)
+
+    return pos, q, Nq 
+
+
+def find_dipole(xq, q):
+    """
+    Finds the dipole moment of the protein. 
+
+    Arguments
+    ---------
+    xq      : (Nqx3) array, positions of the charges.
+    q       : (Nqx1) array, value of the charges.
+
+    Returns
+    -------
+    d       : array, xyz cordinates of the dipole moment. 
+
+    """
+
+    ctr = numpy.sum(numpy.transpose(xq)*abs(q), axis=1)/numpy.sum(abs(q))
+    r = xq - ctr
+
+    d = numpy.sum(numpy.transpose(r)*q, axis=1)
+
+    return d
+
+
+def rotate_x(pos, angle):
+    """ Rotates the coordinates respect to x-axis, by an angle. 
+    
+    Arguments
+    ---------
+    pos   : (Nx3) array, positions.
+    angle : float, angle in radians.  
+
+    Return
+    ------
+    posnew  : (Nx3) array, rotated positions.
+
+    """
+
+    posnew = numpy.zeros(numpy.shape(pos))
+    posnew[:,0] = pos[:,0]
+    posnew[:,1] = pos[:,1]*numpy.cos(angle) - pos[:,2]*numpy.sin(angle)
+    posnew[:,2] = pos[:,1]*numpy.sin(angle) + pos[:,2]*numpy.cos(angle)
+
+    return posnew
+
+def rotate_y(pos, angle):
+    """ Rotates the coordinates respect to y-axis, by an angle. 
+    
+    Arguments
+    ---------
+    pos   : (Nx3) array, positions.
+    angle : float, angle in radians.  
+
+    Return
+    ------
+    posnew  : (Nx3) array, rotated positions.
+
+    """
+
+    posnew = numpy.zeros(numpy.shape(pos))
+    posnew[:,0] = pos[:,2]*numpy.sin(angle) + pos[:,0]*numpy.cos(angle)
+    posnew[:,1] = pos[:,1]
+    posnew[:,2] = pos[:,2]*numpy.cos(angle) - pos[:,0]*numpy.sin(angle)
+
+    return posnew   
+
+def rotate_z(pos, angle):
+    """ Rotates the coordinates respect to z-axis, by an angle. 
+    
+    Arguments
+    ---------
+    pos   : (Nx3) array, positions.
+    angle : float, angle in radians.  
+
+    Return
+    ------
+    posnew  : (Nx3) array, rotated positions.
+
+    """
+
+    posnew = numpy.zeros(numpy.shape(pos))
+    posnew[:,0] = pos[:,0]*numpy.cos(angle) - pos[:,1]*numpy.sin(angle)
+    posnew[:,1] = pos[:,0]*numpy.sin(angle) + pos[:,1]*numpy.cos(angle)
+    posnew[:,2] = pos[:,2]
+
+    return posnew   
+
+def modify_pqr(inpqr, outpqr, xq):
+
+    with open(outpqr, 'w') as file_o:
+        for line in inpqr:
+            line_split = line.split()
+
+            if line_split[0] == 'ATOM':
+                atm_nu = int(line_split[1])-1
+                line_add = ' %3.3f  %3.3f  %3.3f '%(xq[atm_nu,0], xq[atm_nu,1], xq[atm_nu,2])
+                line_new = line[:27] + line_add + line[55:]
+                file_o.write(line_new)
+            else:
+                file_o.write(line)
+    file_o.close()
+
+
---
+++ preprocessing_tools/move_protein.py
@@ -0,0 +1,194 @@
+'''
+Generates mesh and pqr files of a protein that has been translated and rotated.
+Rotation and tilt angles are passed as arguments. 
+Translation has to be modified in script.
+
+                                    y
+Axis are assumed to be oriented as   |__ x
+                                    /
+                                   z  
+'''
+
+import numpy
+import os
+
+from argparse import ArgumentParser
+
+from move_prot_helper import (read_inputs, read_vertex, read_pqr, find_dipole,
+                            rotate_x, rotate_y, rotate_z, modify_pqr)
+
+
+#Reading the inputs:
+
+args = read_inputs()
+
+inMesh  = args.im
+inpqr   = args.ip
+alpha_y = float(args.ay)*numpy.pi/180. 
+alpha_z = float(args.az)*numpy.pi/180.
+name    = args.name
+verbose = args.v
+
+
+outMesh = inMesh + name
+outpqr = inpqr + name
+
+#Read mesh and pqr
+vert = read_vertex(inMesh+'.vert', float)
+xq, q, Nq = read_pqr(inpqr+'.pqr', float)
+
+#### Setup initial configuration
+# Initial configuration: dipole parallel to y and outermost atom to center parallel to x
+d = find_dipole(xq,q)
+normd = numpy.sqrt(sum(d*d))
+normal  = numpy.array([0,1,0])
+normal2 = numpy.array([1,0,0])
+
+angle = numpy.arccos(numpy.dot(d, normal)/normd)
+
+## Align normal and dipole vectors
+
+# Rotate x axis
+angle_x = -numpy.arctan2(d[2],d[1])  # Positive angle gets away from y, then negative to take it back
+xq_aux = rotate_x(xq, angle_x)
+vert_aux = rotate_x(vert, angle_x)
+
+# Rotate z axis
+d_aux = find_dipole(xq_aux, q)
+angle_z = numpy.arctan2(d_aux[0],d_aux[1]) # Positive angle approaches y, then it's ok
+xq_aux2 = rotate_z(xq_aux, angle_z)
+vert_aux2 = rotate_z(vert_aux, angle_z)
+
+## Align vector of atom furthest to center to x axis
+
+# Pick atom
+ctr = numpy.average(xq_aux2, axis=0) 
+r_atom = xq_aux2 - ctr
+r_atom_norm = numpy.sqrt(xq_aux2[:,0]**2+xq_aux2[:,2]**2) # Distance in x-z plane
+max_atom = numpy.where(r_atom_norm==max(r_atom_norm))[0][0]
+
+# Rotate y axis
+r_atom_max = r_atom[max_atom]
+angle_y = numpy.arctan2(r_atom_max[2], r_atom_max[0])
+xq_0 = rotate_y(xq_aux2, angle_y)
+vert_0 = rotate_y(vert_aux2, angle_y)
+
+# Check if dipole and normal are parallel
+d_0 = find_dipole(xq_0, q)
+
+# Check if furthest away atom vector and x axis are parallel
+ctr = numpy.average(xq_0, axis=0) 
+ctr[1] = xq_0[max_atom,1]
+r_atom = xq_0 - ctr
+max_atom_vec = r_atom[max_atom]
+
+
+#If verbose was set to True, it'll print info about dipole alignment.
+
+check_dipole = numpy.dot(d_0,numpy.array([1,1,1]))
+check_atom   = numpy.dot(max_atom_vec,numpy.array([1,1,1]))
+
+if verbose:
+    print('Initial configuration:')
+if abs(check_dipole - abs(d_0[1]))<1e-10: 
+    if verbose: print ('\tDipole is aligned with normal')
+else: print ('\tDipole NOT aligned!')
+if abs(check_atom - abs(max_atom_vec[0]))<1e-10: 
+    if verbose: print ('\tMax atom is aligned with x axis')
+else: print ('\tMax atom NOT aligned!')
+
+### Move to desired configuration
+
+## Rotate
+# Rotate y axis
+xq_aux = rotate_y(xq_0, alpha_y)
+vert_aux = rotate_y(vert_0, alpha_y)
+
+# Rotate z axis
+xq_new = rotate_z(xq_aux, alpha_z)
+vert_new = rotate_z(vert_aux, alpha_z)
+
+## Translate
+xmin = min(vert_new[:,0])
+ymin = min(vert_new[:,1])
+zmin = min(vert_new[:,2])
+
+R_sensor = 250 #Angstrom
+dist = 10 #Angstrom
+y_trans = 0
+x_trans = 0 
+z_trans = R_sensor + dist
+
+ctr = numpy.average(vert_new, axis=0) 
+
+
+translation = numpy.array([ctr[0], ctr[1], zmin - z_trans]) # z_trans Angs apart of th sphere in the z direction
+
+# Move according to translation
+
+vert_new -= translation
+xq_new -= translation
+
+
+## Check
+ctr = numpy.average(vert_new, axis=0) 
+print(ctr)
+d = find_dipole(xq_new, q)
+dx = numpy.array([0, d[1], d[2]])
+dy = numpy.array([d[0], 0, d[2]])
+dz = numpy.array([d[0], d[1], 0])
+normd = numpy.sqrt(sum(d*d))
+normdx = numpy.sqrt(sum(dx*dx))
+normdz = numpy.sqrt(sum(dz*dz))
+angle = numpy.arccos(numpy.dot(d, normal)/normd)
+anglex = numpy.arccos(numpy.dot(dx, normal)/normdx)
+anglez = numpy.arccos(numpy.dot(dz, normal)/normdz)
+
+xq_check = rotate_z(xq_new, -alpha_z)
+ctr_check = numpy.average(xq_check, axis=0)
+atom_vec = numpy.array([xq_check[max_atom,0]-ctr_check[0], 0, xq_check[max_atom,2]-ctr_check[2]])
+atom_vec_norm = numpy.sqrt(sum(atom_vec*atom_vec))
+angley = numpy.arccos(numpy.dot(atom_vec, normal2)/atom_vec_norm)
+
+
+#If verbose was set to True, it'll print isummary of configuration.
+
+if alpha_y>numpy.pi:
+    angley = 2*numpy.pi-angley    # Dot product finds the smallest angle!
+
+if verbose:
+    print ('Desired configuration:')
+if abs(ctr[0])<1e-10 and abs(ctr[1])<1e-10:
+    if verbose:
+        print ('\tProtein is centered, %f angs over the surface in the z direction'%(min(vert_new[:,2])))
+else:
+    print ('\tProtein NOT well located!')
+
+if abs(d[2])<1e-10:
+    if verbose:
+        print ('\tDipole is on x-y plane, %f degrees from normal'%(angle*180/numpy.pi))
+else:
+    print ('\tDipole is NOT well aligned')
+
+if abs(angle-alpha_z)<1e-10:
+    if verbose:
+        print ('\tMolecule was tilted correctly by %f deg'%(angle*180/numpy.pi))
+else:
+    print ('\tMolecule was NOT tilted correctly!')
+
+if abs(angley-alpha_y)<1e-10:
+    if verbose:
+        print ('\tMolecule was rotated correctly %f deg'%(angley*180/numpy.pi))
+else:
+    print ('\tMolecule was NOT rotated correctly!')
+
+
+#### Save to file
+numpy.savetxt(outMesh+'.vert', vert_new)
+cmd = 'cp '+inMesh+'.face '+outMesh+'.face'
+os.system(cmd)
+
+modify_pqr(inpqr+'.pqr', outpqr+'.pqr', xq_new)
+
+if verbose:
+    print ('\nWritten to '+outMesh+'.vert(.face) and '+outpqr+'.pqr')
